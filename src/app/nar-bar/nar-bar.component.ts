f8b591c50d1669a8616fb343bc80bbf477dc7e3f
NguyenNgoHungDung <dung.nnh5930@sinhvien.hoasen.edu.vn>
2023-09-19 14:46:43 +0700

    giao dien

---
+++ src/app/nar-bar/nar-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-nar-bar',
+  templateUrl: './nar-bar.component.html',
+  styleUrls: ['./nar-bar.component.scss']
+})
+export class NarBarComponent {
+  }
+  
+
